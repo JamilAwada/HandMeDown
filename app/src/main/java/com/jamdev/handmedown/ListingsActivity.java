5dfd24a3e2c9e4a74e0bf63168bf1d1f45726f54
Jamil Awada <jamilawada2002@gmail.com>
2022-04-24 03:13:52 +0300

    added fragments

--- app/src/main/java/com/jamdev/handmedown/ListingsActivity.java
+++ app/src/main/java/com/jamdev/handmedown/ListingsActivity.java
@@ -1,20 +1,18 @@
 package com.jamdev.handmedown;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ListingsActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
+public class ListingsActivity extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // These 3 lines hide the title and action bar
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        getSupportActionBar().hide();
-        setContentView(R.layout.activity_listings);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_listings, container, false);
     }
 }
--- app/src/main/java/com/jamdev/handmedown/MainActivity.java
+++ app/src/main/java/com/jamdev/handmedown/MainActivity.java
@@ -1,20 +1,66 @@
 package com.jamdev.handmedown;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.jamdev.handmedown.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // These 3 lines hide the title and action bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
+
+        // Default fragment is home
+        replaceFragment(new HomeActivity());
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Switch between fragments
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    replaceFragment(new HomeActivity());
+                    break;
+                case R.id.nav_search:
+                    replaceFragment(new SearchActivity());
+                    break;
+                case R.id.nav_listings:
+                    replaceFragment(new ListingsActivity());
+                    break;
+                case R.id.nav_profile:
+                    replaceFragment(new ProfileActivity());
+                    break;
+            }
+            return true;
+        });
     }
+
+    // Function used to replaceFragment, takes target fragment as destination
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
